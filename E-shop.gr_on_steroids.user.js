5a8e68f7464240c7e899a24c13489cd2306b4e9f
JimTortex <dimitrisiliascs@gmail.com>
2016-05-08 19:34:17 +0300

    Fixed one more time

--- E-shop.gr_on_steroids.user.js
+++ E-shop.gr_on_steroids.user.js
@@ -1,7 +1,7 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        E-shop.gr on steroids
 // @description Shows ratings without having to open each product's page, removes adds and anything space consuming.
-// @version     1.6
+// @version     1.7
 // @author      JimTortex
 // @include     http*www.e-shop.gr/*
 // @icon        https://media.licdn.com/media/AAEAAQAAAAAAAABGAAAAJGI2MWY0MjE1LTA1MzMtNDVhNi1hMjc3LThmYzk1ZTY1MTU3Yg.png
@@ -92,7 +92,7 @@ function main(){
 
 
 	var current_url = window.location.href;
-	if (current_url.indexOf("list?") > 0){															//if we are on product list mode
+	if (current_url.indexOf("list?") > 0){															//if we are on product listing
 		
 	  function processProductsList(){
 
@@ -102,37 +102,48 @@ function main(){
 				  xmlhttp.onreadystatechange=function()
 				  {
 					if (xmlhttp.readyState==4 && xmlhttp.status==200){
+
 					    var parser = new DOMParser();
 
 					    doc        = parser.parseFromString(xmlhttp.responseText, "text/html");
-					    txt        = doc.getElementsByClassName("comments_table_title")[1].getElementsByTagName("img")[0];
-					    if (txt != undefined){
-						  var src        = txt.getAttribute("src");
-						  var title = doc.getElementsByClassName("comments_table_title")[2].textContent.slice(14,-1);
-						  
-						  var reviewers = doc.getElementById("reviewid_1").getElementsByClassName("comments_table_comment1");
-						  var comments = doc.getElementById("reviewid_1").getElementsByClassName("comments_table_comment2");
+					    txt        = doc.getElementsByClassName("comments_table_title")[0].innerHTML;
+						
+					    if (txt != ""){
 
-						  title = title + " Reviews" + " and " + comments.length + " Comments:";
-						  for (var i = 0; i < comments.length; i++) {
-						  		j = i + 1;
-						 		title = title +"\n\n"+ reviewers[i].textContent + ":\n" + comments[i].textContent;
-						  };
+								var rating        = txt.slice(11);
+								var pixels_width  = rating*104;
+									
+								var title = doc.getElementsByClassName("comments_table_title")[2].textContent.slice(14,-1);
 
-						  var img = document.createElement('img');
-						  img.src = src;
-						  img.title = "";
-						  img.style.zIndex = "10000";
+								var reviewers = doc.getElementById("reviewid_1").getElementsByClassName("comments_table_comment1");
+								var comments = doc.getElementById("reviewid_1").getElementsByClassName("comments_table_comment2");
 
-						  var link = document.createElement('a');
-						  link.style = "margin-left:0.3em;margin-right:0.3em;float:right;";
-						  link.title = title;
-						  link.classList.add("tooltip");
+								title = title + " Reviews" + " and " + comments.length + " Comments:";
+								for (var i = 0; i < comments.length; i++) {
+										j = i + 1;
+									title = title +"\n\n"+ reviewers[i].textContent + ":\n" + comments[i].textContent;
+								};
+
+								var img = document.createElement('img');
+
+								img.src = "https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png";
+								img.style = "background: transparent url('http://images.e-shop.gr/images/web-global.png') no-repeat scroll " + "-" + pixels_width + "px" + " -101px;width: 104px; height: 20px;"
+								img.title = "";
+
+								var link = document.createElement('a');
+								link.style = "margin-left:0.3em;margin-right:0.3em;float:right;";
+								link.title = title;
+								link.classList.add("tooltip");
 
 
-						  link.appendChild(img);
-						  td.appendChild(link);
+								link.appendChild(img);
+
+								td.appendChild(link);
+								 
 					    }
+								if (td.getElementsByTagName("font")[1]){
+										 td.getElementsByTagName("font")[1].setAttribute("color", "#0858a7");
+								}
 					}
 				  }
 				  xmlhttp.open("POST", url);
@@ -144,7 +155,7 @@ function main(){
 		  tdlist = document.getElementById("web_body").getElementsByTagName("td");
 
 		  for (var i = 0; i < tdlist.length; i++) {
-			  if (tdlist[i].getAttribute("class") === "web-product-title"){							//ratings magic
+			  if (tdlist[i].getAttribute("class") === "web-product-title"){							//if class is web-product-title
 				  var href = tdlist[i].getElementsByClassName("web-title-link")[0].href;
 				  httpGet(href , tdlist[i]);
 
